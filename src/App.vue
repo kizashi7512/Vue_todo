b74fb21a90fb2d31c5705f565b3b84ba2e4448ff
kizashi <kizashi3230@gmail.com>
2021-04-01 15:25:06 +0900

    add

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h3>My TODO</h3>
+    <input v-model='newTodo' placeholder="Input here...">
+    <button v-on:click="addTodo()">ADD</button>
+    <h5>ToDo List</h5>
+    <ul>
+      <li v-for='(todo, i) in todos' v-bind:key="i">
+        {{ todo }}
+        <button v-on:click='deleteTodo(i)'>DEL</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      todos: [],
+      newTodo: ''
+    }
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodo === '') return;
+      this.todos.push(this.newTodo);
+      this.newTodo = '';
+    },
+    deleteTodo(i) {
+      this.todos.splice(i, 1);
+    }
+  }
+}
+</script>
+
